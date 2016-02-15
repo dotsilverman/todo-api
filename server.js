1600b6aa1f0beca33676a059a5cc46bd48c3cab8
Dot Silverman <dot.silverman10@gmail.com>
2016-02-15 18:49:20 -0500

    Refactor API with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -18,14 +19,8 @@ app.get('/todos', function (request, response) {
 
 app.get('/todos/:id', function (request, response) {
 	var todoId = parseInt(request.params.id, 10);
-	var matchedTodo;
+	var matchedTodo = _.findWhere(todos, {id: todoId}); 
 
-	todos.forEach(function (todo) {
-		if (todoId === todo.id) {
-			matchedTodo = todo;
-		}
-
-	});
 	if (matchedTodo) {
 		response.json(matchedTodo);
 	} else {
@@ -33,8 +28,17 @@ app.get('/todos/:id', function (request, response) {
 	}
 });
 
+// add a todo item
 app.post('/todos', function (request, response) {
-	var body = request.body;
+	//var body = request.body;
+	var body = _.pick(request.body, 'description', 'completed');
+
+	// catch bad request
+	if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+		return response.status(400).send();
+	}
+
+	body.description = body.description.trim();
 
 	// add id field
 	body.id = todoNextId++;
@@ -43,7 +47,6 @@ app.post('/todos', function (request, response) {
 	todos.push(body);
 
 	// test with a post request and then a get request
-
 	console.log('description: ' + body.description);
 
 	response.json(body);
