e86fcab278d608ab67a25a7558347faa5628427d
Dot Silverman <dot.silverman10@gmail.com>
2016-02-15 22:50:30 -0500

    Final static todos

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/', function (request, response) {
 	response.send('Todo API Root');
 });
 
-// GET /todos?completed=true
+// GET /todos?completed=true&q=work
 app.get('/todos', function (request, response) {
 	var queryParams = request.query;
 	var filteredTodos = todos;
@@ -22,6 +22,12 @@ app.get('/todos', function (request, response) {
 		filteredTodos = _.where(filteredTodos, {completed: true});
 	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
 		filteredTodos =_.where(filteredTodos, {completed: false});
+	}
+
+	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
+		filteredTodos = _.filter(filteredTodos, function (todo) {
+			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
+		});
 	}
 
 	response.json(filteredTodos);
