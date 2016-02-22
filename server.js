5897a84334ac787b607d98f12437844aa090e9dd
Dot Silverman <dot.silverman10@gmail.com>
2016-02-22 04:55:23 -0500

    Add login route and authenticate class method

--- server.js
+++ server.js
@@ -138,12 +138,14 @@ app.post('/users/login', function(request, response) {
 	db.user.authenticate(body).then(function(user) {
 		return response.json(user.toPublicJSON());
 	}, function() {
-		res.status(401).send();
+		response.status(401).send();
 	});
 });
 
 // start the server
-db.sequelize.sync().then(function() {
+db.sequelize.sync({
+	force: true
+}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
